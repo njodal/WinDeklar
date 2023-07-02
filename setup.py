ec2427950468b23c017c698102f7026c1daa16fe
Njodal <jnj@genexus.com>
2023-07-02 16:27:52 -0300

    Better graph positioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='WinDeklar',
-    version='0.2.1',
+    version='0.2.2',
     description='Create winforms in an easy, declarative way. Specially suited for Robotics applications.',
     author='Nicolas Jodal',
     author_email='jnj@genexus.com',
--- src/WinDeklar/WindowForm.py
+++ src/WinDeklar/WindowForm.py
@@ -191,8 +191,8 @@ class FigureView(FigureCanvas):
 
         FigureCanvas.setSizePolicy(self, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         FigureCanvas.updateGeometry(self)
-        self.figure.tight_layout()
-        # self.figure.subplots_adjust(left=0.2, bottom=0.2, right=0.8, top=0.8, wspace=0.2, hspace=0.5)
+        # self.figure.tight_layout()
+        self.figure.subplots_adjust(left=0.1, right=0.9, bottom=0.1, top=0.9)
 
         self.figure.canvas.mpl_connect('button_press_event', self.onclick)
         self.figure.canvas.mpl_connect('motion_notify_event', self.on_mouse_move)
